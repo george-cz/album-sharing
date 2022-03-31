a005e3c7678d6b19a413e285c8e9215955e8017d
Jiri Vyhnalek <jiri@vyhnalek.net>
2022-03-31 16:11:35 +0200

    initial commit

---
+++ components/Search.tsx
@@ -0,0 +1,9 @@
+const Search = () => {
+  return (
+    <div className="mb-4 shadow-md  bg-white rounded-md p-1">
+      <input type="text" className="px-6 py-4 " />
+    </div>
+  );
+};
+
+export default Search;
