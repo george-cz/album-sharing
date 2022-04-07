052394f6619a4d506358806f4e13698e6e36baca
Jiri Vyhnalek <jiri@vyhnalek.net>
2022-04-07 20:16:27 +0200

    add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+
+const GA = () => (
+  <>
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-14S8MJFT41"
+      async
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-14S8MJFT41');
+        `}
+    </Script>
+  </>
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GA />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
